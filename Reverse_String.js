580dc43d0618793ecad200e16e9067cd0293bfc9
priyanka <priyankachandimas@gmail.com>
2022-07-19 20:19:13 +0530

    Add string revers

---
+++ Reverse_String.js
@@ -0,0 +1,20 @@
+//Revers string using arrays
+
+let inputStringOne = "NimalKamal";
+
+console.log("Revers string of " + inputStringOne + " is "+ getReversString(inputStringOne) +"")
+
+function getReversString(string){
+    let reversString =  string.split('').reverse().join('')
+    return reversString
+}
+
+console.log("Revers string of " + inputStringOne + " is "+ getReversStringUsingCarAt(inputStringOne) +"")
+
+function getReversStringUsingCarAt(string){
+    let reverse = ""
+    for(let x= string.length; x>=0; x--){
+        reverse = reverse + string.charAt(x)
+    }
+    return reverse
+}
